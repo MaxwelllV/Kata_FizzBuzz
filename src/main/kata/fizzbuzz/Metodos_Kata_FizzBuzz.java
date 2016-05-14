7fc4204793ac7b31031ad6dab16d953b075c6c5f
Jorge Dominguez <jrethekyngg@gmail.com>
2016-05-14 22:58:16 +0200

    Modificado test setUp sin throw, modificado Metodos_kata

--- src/main/kata/fizzbuzz/Metodos_Kata_FizzBuzz.java
+++ src/main/kata/fizzbuzz/Metodos_Kata_FizzBuzz.java
@@ -8,6 +8,16 @@ package main.kata.fizzbuzz;
  *
  */
 public class Metodos_Kata_FizzBuzz {
+	
+	/**
+	 * This method receives a number as parameter and checks if is multiple of 3, 5 or both.
+	 * In case that the number is multiple of 3 or , returns 'Fizz'
+	 * In case that the number is multiple of 5, returns 'Buzz'
+	 * In case that the number is multiple of 3 and 5, returns 'FizzBuzz'
+	 * In case that the number isn't multiple, returns the number
+	 * @param num: The given number
+	 * @return : 'Fizz' or 'Buzz' or 'FizzBuzz'
+	 */
 
 	public String numeros(int num){
 		String result = ""+num;
--- test/test/kata/fizzbuzz/Test_kata_fizzbuzz.java
+++ test/test/kata/fizzbuzz/Test_kata_fizzbuzz.java
@@ -17,22 +17,45 @@ import main.kata.fizzbuzz.Metodos_Kata_FizzBuzz;
 
 public class Test_kata_fizzbuzz {
 
+	/**
+	 * A variable of Metodos_Kata_FizzBuzz
+	 */
+	
 	private Metodos_Kata_FizzBuzz kfb;
 	
+	/**
+	 * Keep in the variable a new object of Metodos_Kata_FizzBuzz
+	 */
+	
 	@Before
-	public void setUp() throws Exception{
+	public void setUp(){
 		kfb = new Metodos_Kata_FizzBuzz();
 	}
+	
+	/**
+	 * This test checks if the given number for parameter is equals 'Fizz'
+	 */
+	
 	@Test
 	public void numerosentretres(){
 		String result = kfb.numeros(3);
 		assertEquals("Fizz", result);
 	}
+	
+	/**
+	 * This test checks if the given number for parameter is equals 'Buzz'
+	 */
+	
 	@Test
 	public void numeroscinco(){
 		String result = kfb.numeros(5);
 		assertEquals("Buzz", result);
 	}
+	
+	/**
+	 * This test checks if the given number for parameter is equals 'FizzBuzz'
+	 */
+	
 	@Test
 	public void numerostresecinco(){
 		String result = kfb.numeros(15);
